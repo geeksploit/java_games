219bffc05eb73241f5267cae107e401c85d9564a
geeksploit <geeksploit@shootan.com>
2017-09-06 19:07:56 +0300

    Create the StarField class to easily manage a bunch of stars altogether.

---
+++ core/src/ru/geekbrains/java_games/screens/stars/StarField.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.java_games.screens.stars;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
+
+import ru.geekuniversity.engine.math.Rect;
+
+/**
+ * Created by geeksploit on 06.09.2017.
+ */
+
+public class StarField {
+
+    public static final int DEFAULT_STARS_AMOUNT = 250;
+    private static final float STAR_DEFAULT_HEIGHT = 0.01f;
+
+    Array<Star> stars;
+
+    public StarField(TextureAtlas atlas) {
+        stars = new Array<Star>(DEFAULT_STARS_AMOUNT);
+        TextureRegion regionStar = atlas.findRegion("star");
+        while (stars.size < DEFAULT_STARS_AMOUNT) {
+            float vx = MathUtils.random(-0.005f, 0.005f);
+            float vy = MathUtils.random(-0.05f, -0.1f);
+            float starHeight = STAR_DEFAULT_HEIGHT * MathUtils.random(0.75f, 1f);
+            stars.add(new Star(regionStar, vx, vy, starHeight));
+        }
+    }
+
+    public void resize(Rect worldBounds) {
+        for (int i = 0; i < stars.size; i++) {
+            stars.get(i).resize(worldBounds);
+        }
+    }
+
+    public void draw(SpriteBatch spriteBatch) {
+        for (int i = 0; i < stars.size; i++) {
+            stars.get(i).draw(spriteBatch);
+        }
+    }
+
+    public void update(float deltaTime) {
+        for (int i = 0; i < stars.size; i++) {
+            stars.get(i).update(deltaTime);
+        }
+    }
+}
