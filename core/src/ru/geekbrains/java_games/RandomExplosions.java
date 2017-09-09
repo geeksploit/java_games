8a3cde56c197ed04fe4a539058878d7714a16a4c
geeksploit <geeksploit@shootan.com>
2017-09-09 16:28:45 +0300

    Add RandomExplosions class to handle background random explosions.

---
+++ core/src/ru/geekbrains/java_games/RandomExplosions.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.java_games;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import ru.geekbrains.java_games.pools.ExplosionPool;
+import ru.geekuniversity.engine.math.Rect;
+
+/**
+ * Created by geeksploit on 09.09.2017.
+ */
+
+public class RandomExplosions {
+
+    public RandomExplosions(ExplosionPool explosionPool, int i) {
+    }
+
+    public void resize(Rect worldBounds) {
+    }
+
+    public void update(float deltaTime) {
+    }
+
+    public void draw(SpriteBatch batch) {
+    }
+}
