5ce3715c656409d2e961791def175e18b694a0e7
geeksploit <geeksploit@shootan.com>
2017-09-10 15:01:40 +0300

    Create the Enemy class stub.

---
+++ core/src/ru/geekbrains/java_games/Enemy.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.java_games;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Created by geeksploit on 10.09.2017.
+ */
+
+public class Enemy extends Ship {
+
+    public Enemy(TextureRegion region) {
+        super(region, 1, 2, 2);
+    }
+
+}
