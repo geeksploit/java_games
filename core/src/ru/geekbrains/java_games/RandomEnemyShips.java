5bb3260e3d5016d1601b8ab13c1f0c28796d829c
geeksploit <geeksploit@shootan.com>
2017-09-10 15:33:29 +0300

    Implement the custom constructor accepting a spawn pool and a spawn timeout.

--- core/src/ru/geekbrains/java_games/RandomEnemyShips.java
+++ core/src/ru/geekbrains/java_games/RandomEnemyShips.java
@@ -1,9 +1,19 @@
 package ru.geekbrains.java_games;
 
+import ru.geekbrains.java_games.pools.EnemyPool;
+
 /**
  * Created by geeksploit on 10.09.2017.
  */
 
 public class RandomEnemyShips {
+
+    private EnemyPool spawnPool;
+    private float spawnTimeout;
+
+    public RandomEnemyShips(EnemyPool spawnPool, float spawnTimeout) {
+        this.spawnPool = spawnPool;
+        this.spawnTimeout = spawnTimeout;
+    }
 
 }
